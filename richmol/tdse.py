b70c7eabe94cb7347c7e6145a6f058fed85fa25d
Andrey <andrey.yachmenev@desy.de>
2025-05-12 07:55:35 +0200

    Add docstring for propagate_rk

--- richmol/tdse.py
+++ richmol/tdse.py
@@ -20,11 +20,70 @@ def propagate_rk(
     time_unit: str,
     c0: np.ndarray,
     t_eval: Optional[np.ndarray] = None,
-):
+) -> tuple[np.ndarray, np.ndarray]:
+    """Computes time-dependent wavepacket coefficients by solving the
+    time-dependent Schrödinger equation for a molecule interacting
+    with an external field, using the Runge-Kutta method
+    from SciPy's `solve_ivp`.
+
+    The total Hamiltonian includes:
+        - A field-free component specified by rotational or rovibrational
+        energy levels (`states`), in cm^{-1}.
+        - A field-dependent component consisting of a linear combination of
+        Cartesian tensor operators (`tens_op`) scaled by prefactors
+        (`tens_prefac`) and the time-dependent external field (`field_func`).
+
+    Args:
+        states (RotStates):
+            Rotational or rovibrational energy states forming the basis for
+            the wavepacket. Energies must be in units of cm^{-1}.
+
+        tens_op (list[CartTensor]):
+            List of Cartesian tensor operators (e.g., dipole moment, polarizability)
+            that couple to the external field.
+
+        tens_prefac (list[float]):
+            List of scaling factors corresponding to each tensor in `tens_op`.
+            These should convert the products of tensors with field into energy
+            units of cm^{-1}.
+
+        field_func (Callable[[float], np.ndarray]):
+            Function that returns a 1D array of shape (3,) representing the
+            X, Y, and Z components of the external electric field at a given time.
+            Ensure that the product of `field_func` and `tens_op` scaled by
+            `tens_prefac` yields a Hamiltonian in cm^{-1}.
+            The input time is in the unit specified by `time_unit`.
+
+        t0 (float):
+            Initial time of propagation.
+
+        t1 (float):
+            Final time of propagation.
+
+        time_unit (str):
+            Unit of time used for `t0`, `t1`, `t_eval` and values passed
+            to `field_func`. Supported values  include, e.g., "fs" (femtoseconds),
+            "ps" (picoseconds), or "ns" (nanoseconds).
+
+        c0 (np.ndarray):
+            Initial wavepacket coefficients at time `t0`.
+
+        t_eval (Optional[np.ndarray]):
+            Optional array of time points at which to evaluate and store the solution.
+            If not provided, defaults to a uniform grid of 1000 points between
+            `t0` and `t1`.
+
+    Returns:
+        tuple[np.ndarray, np.ndarray]:
+            A tuple containing:
+            - `t_eval`: Array of time points.
+            - `c_t`:Array of complex wavepacket coefficients at each time in
+            `t_eval`, with shape (len(t_eval), len(c0)).
+    """
     time_units = {"ps": 1e-12, "fs": 1e-15, "ns": 1e-9}
     assert time_unit.lower() in time_units, (
         f"Unknown value for 'time_unit' = {time_unit}, "
-        + f"accepted values = {list(time_units.keys())}"
+        + f"accepted values: {list(time_units.keys())}"
     )
 
     h0 = states.mat()
