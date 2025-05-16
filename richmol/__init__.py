7d4526f139a200c6a95126ea7beee2c01f538ccc
Andrey <andrey.yachmenev@desy.de>
2025-05-16 21:35:08 +0200

    Modify __init__

--- richmol/__init__.py
+++ richmol/__init__.py
@@ -1,4 +1,6 @@
 from .asymtop import RotStates
 from .cartens import CartTensor
+from .hyperfine import HyperStates, HyperCartTensor
+from .nucspin import QuadMom, Spin
 from .constants import *
 from .tdse import propagate_rk
